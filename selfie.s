6d1c1afddedbec71bda395af713285b42f46dec1
Yannik <115665549+yannikosterholzer@users.noreply.github.com>
2023-03-25 22:36:33 +0100

    Update selfie.s

--- selfie.s
+++ selfie.s
@@ -12,18 +12,20 @@ _start:
 // MAIN
 // -------------------------------------
 
-	selfrepl:	
-	mov r3, #0	     // counter initialisieren							
-	sub r0, pc, #12	     // Pointer x auf Programmanfang 																	
-	ldr r1, [r0]	     // Lade Befehl bei x -> y						
-	str r1, [r0, #32]    // Pointer x + 32 auf Programmende					    
-	add r3, r3, #1	     // counter++							
-	add r0, r0, #4	     // x++							
-	cmp r3, #9	     // if(counter != 9)						    
-	subne pc, pc, #28    //    	-> jmp to selfrep		   					
-.space 20000
+selfrepl:
+	mov r3, #10 		// Counter initialisieren
+	sub r0, pc, #12 	// Pointer x auf Programmanfang setzen
+loop:
+	ldr r1, [r0]    	// Lade Befehl bei x -> y
+	str r1, [r0, #28] 	// Kopiere Befehl von y nach z
+	add r0, r0, #4 		// x++
+	subs r3, r3, #1 	// counter--
+	subne pc, pc, #24
+	
+	.space 20000
 _end:	
 	b end
+
 	
 //************************************* E O F ******************************************/
 
