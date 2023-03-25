7cad4070506b54f8838dd4bb5ec6c95f76b28723
Yannik <115665549+Ewomb@users.noreply.github.com>
2023-03-25 17:54:08 +0100

    Update F-alloc.s

--- F-alloc.s
+++ F-alloc.s
@@ -48,7 +48,7 @@ mem:
 //------------------------------------
 buffer:
 .space 400
-
+//--------------------------------------------------------------
 
 
 
@@ -151,7 +151,7 @@ req_mem:
 	ldr r0, =tabelle
     mov r1, #0
 	ldr r2, =0xffffffff
-	ldr r3,=800          // ---------------> Tabellengröße
+	ldr r3,=800                // ---------------> Tabellengröße
 	add r3, r0, r3
 req_loop:
 	cmp r0, r3
@@ -162,13 +162,13 @@ req_loop:
 	add r0, r0, #8
 	b req_loop
 ret_mem:
-	str r2, [r0]         // R2 = 0xffffffff
+	str r2, [r0]               // R2 = 0xffffffff
 	add r0, r0, #4
 	ldr r0, [r0]
 	b quit_req
 no_mem:
 	ldr r0, =0xffffffff
-quit_req:              // returnwert in r0
+quit_req:                          // returnwert in r0
 		                   // kein freier Speicher -> r0 = 0xffffffff
 	pop {r4-r12,lr}
 	bx lr
@@ -184,33 +184,33 @@ quit_req:              // returnwert in r0
 free_mem:
     push {r4-r12, lr}
 //------------------------------------ Initialisiere Register
-    mov r1, r0          // R1 = ptr x auf freizugebenden Speicherbereich
-    ldr r2, =tabelle	  //
-    add r1, r1, #40		  // R1 = ptr x + 40 -> zeigt auf Ende einer "Struct" 
-	                      // 10 * 4 Bytes sollen überschrieben werden
-    ldr r3, =0xaaaaaaaa // Wert mit denen der Bereich in Mem überschrieben wird
+    mov r1, r0                      // R1 = ptr x auf freizugebenden Speicherbereich
+    ldr r2, =tabelle	  
+    add r1, r1, #40		    // R1 = ptr x + 40 -> zeigt auf Ende einer "Struct" 
+	                            // 10 * 4 Bytes sollen überschrieben werden
+    ldr r3, =0xaaaaaaaa             // Wert mit denen der Bereich in Mem überschrieben wird
 
-    push {r0}			      // 
+    push {r0}			     
 fm_overwrite:
     cmp r0, r1			    // Kompletter Bereich überschrieben?
-    bhs sp_over_end		  // wenn ja -> sp_over_end	 
+    bhs sp_over_end		    // wenn ja -> sp_over_end	 
     str r3, [r0]		    // *ptr = 0xaaaaaaaa
-    add r0, r0, #4		  //
-    b fm_overwrite		  //
+    add r0, r0, #4		    //
+    b fm_overwrite		    //
 //-----------------------  Initialsiere Search_ptr
-						            // Suche Ptr auf freizugebenden Speicherbereich 
-						            // in der Speicherverwaltungstabelle "Tabelle"
+				    // Suche Ptr auf freizugebenden Speicherbereich 
+				    // in der Speicherverwaltungstabelle "Tabelle"
 sp_over_end:			
-	ldr r3, =800  //-------> Tabellengröße
-    add r3, r2, r3		  // R3 = R2 (=tabelle) + 800 -> Ptr auf (Tabellen-Ende + 4 Bytes)
+	ldr r3, =800                //-------> Tabellengröße
+    add r3, r2, r3		    // R3 = R2 (=tabelle) + 800 -> Ptr auf (Tabellen-Ende + 4 Bytes)
 	pop {r0}			
 search_ptr:
-	cmp r2, r3			      // Ist Tabellen-Ende erreicht?
+	cmp r2, r3		    // Ist Tabellen-Ende erreicht?
     bhs quit_free		    // Wenn ja -> Abbruch
-    ldr r1, [r2, #4]	  // Lade Ptr y aus Tabelle
+    ldr r1, [r2, #4]	            // Lade Ptr y aus Tabelle
     cmp r1, r0			    // Ist ptr x == ptr y ???
-    beq change_fm	     	// Wenn ja -> change_fm: Ändere den Belegungs-Eintrag
-    add r2, r2, #8		  // Wenn nein -> gehe die Tabelle weiter durch
+    beq change_fm	     	    // Wenn ja -> change_fm: Ändere den Belegungs-Eintrag
+    add r2, r2, #8		    // Wenn nein -> gehe die Tabelle weiter durch
     b search_ptr		    //              bis Abbruchbedingung erreicht
 
 //-----------------------  Ändere Belgungs-Status in der Tabelle
@@ -233,8 +233,8 @@ quit_free:
 // Kurzbeschreibung:
 
 // Die Funktion "test1" testet die korrekte Funktionsweise der Speicherverwaltungsfunktion "req_mem". 
-// Das Testprogramm fordert Speicherplatz an und speichert einen Zeiger auf den erhaltenen Speicher in einem Buffer. 
-// Wenn der Speicherplatz nicht verfügbar ist, dann erhält Test 1 von F-alloc einen Null-pointer und wird beendet.
+// Das Testprogramm fordert so oft wie möglich Speicherplatz an und speichert einen Zeiger auf den erhaltenen Speicher in einem Buffer. 
+// Wenn kein Speicherplatz (mehr) verfügbar ist, dann erhält Test 1 von F-alloc einen Null-pointer und wird beendet.
 
 // Die Funktion "test2" testet die korrekte Funktionsweise der Speicherverwaltungsfunktion "free_mem". 
 // Diese Funktion gibt zuvor angeforderten Speicherplatz frei. 
@@ -242,10 +242,7 @@ quit_free:
 
 
 //--------------------  **TEST 1** : Prüfe ob Speicher alloziert wurde 
-//                             und Tabelleneintrag geändert wurde
-//
-//	Reserviere so viel Speicher wie möglich
-//	um zu prüfen ob Speicherverwaltungstabelle funktioniert
+//                                   und Tabelleneintrag geändert wurde
 //
 //---------------------->  Initialisiere Register für Testfunktion 1
 test1:
@@ -253,13 +250,13 @@ test1:
 	mov r0, #0
 	ldr r1, =buffer
 	mov r2, #0
-    ldr r3, =0xffffffff  // r3 = NULL 
+        ldr r3, =0xffffffff          // r3 = NULL 
 
 test1_loop:	
 	push {r1-r3}
 	bl req_mem
 	pop {r1-r3}
-	cmp r0, r3			     // Gibt req_mem Null zurück?
+	cmp r0, r3		     // Gibt req_mem Null zurück?
 	beq end_test1		     // Wenn ja -> beende Test 1
 	str r0, [r1, r2]
 	add r2, r2, #4
@@ -271,7 +268,7 @@ end_test1:
 
 //-------------------- **TEST 2** : Prüfe ob free(ptr) funktioniert
 //                             und Tabelleneintrag geändert wurde
-test2:					       // r1 muss als Offset übergeben werden 
+test2:			             // r1 muss als Offset übergeben werden 
     push {r4-r12,lr}
     ldr r0, =buffer
     //mov r1, #20 // 6.er Eintrag
@@ -281,3 +278,4 @@ test2:					       // r1 muss als Offset übergeben werden
     pop {r2-r3}
     pop {r4-r12,lr}
     bx lr
+//* ************************************ E O F ***************************************** */
